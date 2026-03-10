b4ec7c831a3773860c98340f3791873f8123354f
Samuel Legrain <samuel.legrain@bstorm.be>
2026-03-10 10:09:33 +0100

    Component Lazy-loading

--- src/app/pages/exos/exos.routes.ts
+++ src/app/pages/exos/exos.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from "@angular/router";
 import { ExoIntro } from "./components/exo-intro/exo-intro";
-import { Exo01Route } from "./components/exo01-route/exo01-route";
 
 export const routes : Routes =[
         {path: 'intro', component : ExoIntro},
-        {path: 'routeComponent', component : Exo01Route},
+        {path: 'routeComponent', 
+            loadComponent : () => import('./components/exo01-route/exo01-route').then(c => c.Exo01Route)},
         {path : '', redirectTo : 'intro', pathMatch : 'full'}
     ];
