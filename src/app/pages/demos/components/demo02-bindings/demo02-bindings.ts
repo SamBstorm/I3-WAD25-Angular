5d235121603e98c20747cfadae7f421bee725f36
Samuel Legrain <samuel.legrain@bstorm.be>
2026-03-10 11:27:37 +0100

    Demo Bindings

---
+++ src/app/pages/demos/components/demo02-bindings/demo02-bindings.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-demo02-bindings',
+  imports: [FormsModule],
+  templateUrl: './demo02-bindings.html',
+  styleUrl: './demo02-bindings.css',
+})
+export class Demo02Bindings {
+
+  public username : string;
+  public bindingSymbols : string = "{{...}}";
+  public favoriteColor? : string;
+  public count : number = 0;
+  public isSwitch : boolean = true;
+
+  constructor(){
+    this.username = "Samuel";
+  }
+
+  public onClickAddOne() : void{
+    this.count++;
+  }
+
+  public onClickSwitchDisabled() : void{
+    this.isSwitch = !this.isSwitch;
+  }
+}
--- src/app/pages/demos/demos.routes.ts
+++ src/app/pages/demos/demos.routes.ts
@@ -3,5 +3,8 @@ import { Demo01Intro } from "./components/demo01-intro/demo01-intro";
 
 export const routes : Routes = [
         {path : 'intro', component : Demo01Intro},
+        {path : 'bindings', 
+            loadComponent : () => import('./components/demo02-bindings/demo02-bindings').then(c => c.Demo02Bindings)
+        },
         {path : '', redirectTo : 'intro', pathMatch : 'full'}
     ];
