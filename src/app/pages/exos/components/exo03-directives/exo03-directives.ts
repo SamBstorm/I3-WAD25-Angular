8d505d58f0c07efeb5e1bfd604b5e5c6731fa0b0
Samuel Legrain <samuel.legrain@bstorm.be>
2026-03-11 12:14:22 +0100

    Exo03 solution avec $index

--- src/app/pages/exos/components/exo03-directives/exo03-directives.ts
+++ src/app/pages/exos/components/exo03-directives/exo03-directives.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import { IAnimal } from '../../../../shared/models/ianimal';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-exo03-directives',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './exo03-directives.html',
   styleUrl: './exo03-directives.css',
 })
-export class Exo03Directives {}
+export class Exo03Directives {
+  public animalSpecies : string[] = ['Chat','Chien','Lapin'];
+
+  public animalName? : string;
+  public animalSpecy : string = 'none';
+
+  public animals : IAnimal[] = [];
+
+  public onClickAjouterAnimal(): void{
+    if(!this.animalName || this.animalName.trim().length <= 0) throw new Error('L\'animal doit avoir un nom.');
+    if(this.animalSpecy == 'none') throw new Error('L\'animal doit avoir une espèce.');
+    let animal : IAnimal = {
+      name : this.animalName,
+      specy : this.animalSpecy,
+      isWalking : false
+    };
+    this.animals.push(animal);
+    this.animalName = undefined;
+    this.animalSpecy = 'none';
+  }
+
+  public onClickSwitchWalking(index : number) : void{
+    this.animals[index].isWalking = !this.animals[index].isWalking;
+  }
+}
