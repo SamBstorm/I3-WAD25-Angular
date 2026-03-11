dfcc7b60d6c473d811529b5d0547c411c08d31f1
Samuel Legrain <samuel.legrain@bstorm.be>
2026-03-11 11:45:06 +0100

    Exo03 Intialisation Route

---
+++ src/app/pages/exos/components/exo03-directives/exo03-directives.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exo03-directives',
+  imports: [],
+  templateUrl: './exo03-directives.html',
+  styleUrl: './exo03-directives.css',
+})
+export class Exo03Directives {}
--- src/app/pages/exos/exos.routes.ts
+++ src/app/pages/exos/exos.routes.ts
@@ -7,5 +7,7 @@ export const routes : Routes =[
             loadComponent : () => import('./components/exo01-route/exo01-route').then(c => c.Exo01Route)},
         {path: 'chrono', 
             loadComponent : () => import('./components/exo02-chrono/exo02-chrono').then(c => c.Exo02Chrono)},
+        {path: 'animal', 
+            loadComponent : () => import('./components/exo03-directives/exo03-directives').then(c => c.Exo03Directives)},
         {path : '', redirectTo : 'intro', pathMatch : 'full'}
     ];
